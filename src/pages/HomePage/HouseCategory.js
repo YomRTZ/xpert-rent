e21600fd92bb189c2ccdeb5d8b8faf83b70849c9
Infinity <infinity@Infinitys-Mac-mini-2.local>
2025-05-07 15:26:37 +0300

    new

--- src/pages/HomePage/HouseCategory.js
+++ src/pages/HomePage/HouseCategory.js
@@ -1,15 +1,46 @@
 import React from 'react';
-import { ScrollView, StyleSheet} from 'react-native';
-import CircularCards from '../../components/CircularCards';
-
+import { ScrollView, StyleSheet,} from 'react-native';
+import { useTheme } from '../../context/ThemeContext';
 const HouseCategory = () => {
+    const {theme}=useTheme();
+    const styles = StyleSheet.create({
+      container: {
+        padding: 10,
+      },
+      categoryItem: {
+        alignItems: 'center',
+        marginRight: 20,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        borderRadius: 5,
+        paddingVertical: 5,
+        paddingHorizontal: 10
+  
+      },
+      categoryImage: {
+        width: 60,
+        height: 60,
+        borderRadius: 40,
+        // marginBottom: 8,
+      },
+      categoryName: {
+        fontSize: 14,
+        fontWeight: '700',
+        color: theme.colors.title,
+        fontFamily:theme.typography.fontFamily.regular,
+        marginLeft: 5
+      },
+    });
   return (
-    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-             <CircularCards name="Apartment" image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeSmLiT8oEm17hs0met89cO8BaqdFvNChnWw&s" />
-             <CircularCards name="Condominium" image="https://borkena.com/wp-content/uploads/2022/08/Addis-Ababa-Condominium-_-Addis-Ababa.jpg"/>
-             <CircularCards name="Ground house " image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm9P4aZsiiNYkVAZf9Ln-y74gNzdQRYXVk0Q&s"/>
-             <CircularCards name="Rooms" image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpZ884xf6gPqKdq1YGQD4aao9ws3YDiVdufg&s" />
-    </ScrollView>
+    <ScrollView style={styles.container} horizontal showsHorizontalScrollIndicator={false}>
+    <TouchableOpacity style={styles.categoryItem}>
+      <View style={styles.categoryIcon}>
+      
+      </View>
+      <Text style={styles.categoryName}>{name}</Text>
+    </TouchableOpacity>
+</ScrollView>
   );
 };
 const styles = StyleSheet.create({
